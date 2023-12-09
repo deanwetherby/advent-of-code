31f6a31eb9bfbf5f69963ffee9b4b52a221a6344
deanwetherby <dean.wetherby@ieee.org>
2023-12-09 10:41:26 -0500

    cleanup

--- 2023/day09/part2.py
+++ 2023/day09/part2.py
@@ -9,9 +9,5 @@ def prev_in_sequence(sequence: list[int]) -> list[int]:
     diff_seq = list(map(lambda x, y: y-x, sequence[:-1], sequence[1:]))
     return [sequence[0] - prev_in_sequence(diff_seq)[0]] + sequence
 
-ct = 0
-for d in data:
-    di = list(map(int, d.split()))
-    ct += prev_in_sequence(di)[0]
 
-print(ct)
+print(sum(prev_in_sequence(list(map(int, d.split())))[0] for d in data))
